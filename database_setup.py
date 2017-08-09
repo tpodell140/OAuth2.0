19e8344d3faad69872435c520d8cfef5b76b90f9
vagrant <vagrant@vagrant.vm>
2017-08-09 04:47:36 +0000

    OAuth functionality, in progress

--- database_setup.py
+++ database_setup.py
@@ -2,14 +2,36 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
- 
+
 Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    name = Column(String(250), nullable = False)
+    email = Column(String(250), nullable = False)
+    picture = Column(String(250))
+    id = Column(Integer, primary_key = True)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializable format"""
+        return {
+            'name'      : self.name,
+            'email'     : self.email,
+            'picture'   : self.picture,
+            'id'        : self.id
+        }
+
 
 class Restaurant(Base):
     __tablename__ = 'restaurant'
-   
+
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
@@ -18,10 +40,11 @@ class Restaurant(Base):
            'name'         : self.name,
            'id'           : self.id,
        }
- 
+
+
+
 class MenuItem(Base):
     __tablename__ = 'menu_item'
-
 
     name =Column(String(80), nullable = False)
     id = Column(Integer, primary_key = True)
@@ -30,6 +53,8 @@ class MenuItem(Base):
     course = Column(String(250))
     restaurant_id = Column(Integer,ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
 
     @property
@@ -45,7 +70,7 @@ class MenuItem(Base):
 
 
 
-engine = create_engine('sqlite:///restaurantmenu.db')
- 
+engine = create_engine('sqlite:///restaurantmenuwithusers.db')
+
 
 Base.metadata.create_all(engine)
